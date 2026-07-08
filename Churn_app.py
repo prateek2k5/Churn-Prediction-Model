c031c85bfd91317ede00f9c5f45c82a3c3ea6e24
Prateek Verma <vermaprateeklive@gmail.com>
2026-07-08 23:16:19 +0530

    Add files via upload

--- Churn_app.py
+++ Churn_app.py
@@ -11,17 +11,17 @@ st.title("Customer Churn Prediction")
 #Subheading of the Webpage
 st.write("Enter the Customer Details below : ")
 
-#Using slider we are taking input from the users (min,max,mid)values
-CreditScore=st.slider("Credit Score",0,1000,500)
-Geography=st.slider("Geography (0-->France 1-->Germany 2-->Spain)",0,2,1)
-Gender=st.slider("Gender (1-->Male , 0-->Female)",0.0,1.0,0.5)
-Age=st.slider("Age",18,100,41)
-Tenure=st.slider("Tenure",0,100,50)
-Balance=st.slider("Balance",0,10000000,5000000)
-NumOfProducts=st.slider("Num_Of_Products_Owned",0,100,50)
-HasCrCard=st.slider("Has_Credit_Card (0-->No 1-->Yes)",0,1,0.5)
-IsActiveMember=st.slider("Is_Active_Member (0-->No 1-->Yes)",0,1)
-EstimatedSalary=st.slider("Estimated_Salary",0,10000000,5000000)
+# User manually enters the values
+CreditScore = st.number_input("Credit Score", min_value=0, max_value=1000, value=500)
+Geography = st.number_input("Geography (0 = France, 1 = Germany, 2 = Spain)",min_value=0, max_value=2, value=0)
+Gender = st.number_input("Gender (0 = Female, 1 = Male)",min_value=0, max_value=1, value=0)
+Age = st.number_input("Age", min_value=18, max_value=100, value=41)
+Tenure = st.number_input( "Tenure", min_value=0, max_value=10, value=5)
+Balance = st.number_input("Balance", min_value=0.0, max_value=250000.0, value=50000.0)
+NumOfProducts = st.number_input("Number of Products", min_value=1, max_value=4, value=1)
+HasCrCard = st.number_input("Has Credit Card (0 = No, 1 = Yes)",min_value=0, max_value=1, value=1)
+IsActiveMember = st.number_input("Is Active Member (0 = No, 1 = Yes)",min_value=0, max_value=1, value=1)
+EstimatedSalary = st.number_input("Estimated Salary",min_value=0.0,max_value=200000.0,value=50000.0)
 
 
 features=np.array([[CreditScore,Geography,Gender,Age,Tenure,Balance,NumOfProducts,HasCrCard,IsActiveMember,EstimatedSalary]])
